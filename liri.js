9848048bcd270e4928092532f1f7d2eed52d0fdf
Israel Harvin <izzy84.ih@gmail.com>
2019-07-27 00:48:10 -0400

    updated liri.js, still needs fixing

--- liri.js
+++ liri.js
@@ -17,8 +17,8 @@ switch (catchACase) {
     spotifyThisSong()
     break;
     case "movie-this":
-    break;
     movieThis()
+    break;
     case "do-what-it-says":
     doWhatItSays()
     break;        
@@ -38,16 +38,11 @@ function concertThis() {
 // The song's name
 // A preview link of the song from Spotify
 // The album that the song is from
-function spotify() {
+function spotifyThisSong() {
     var Spotify = require('node-spotify-api');
 
     // You should then be able to access your keys information like so
     var spotify = new Spotify(keys.spotify);
-
-    var spotify = new Spotify({
-        id: SPOTIFY_ID,
-        secret: SPOTIFY_SECRET,
-    });
 
     spotify.search({ type: 'track', query: 'I Want It That Way' }, function(err, data) {
         if (err) {
@@ -58,46 +53,69 @@ function spotify() {
 }
 
 
-
-
-
-
-// // Basic Node application for requesting data from the OMDB website via axios
-// // Here we incorporate the "axios" npm package
-// var axios = require("axios");
-
-// // We then run the request with axios module on a URL with a JSON
-// axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=2e268f65")
-// .then(
-//   function(response) {
-//     // Then we print out the imdbRating
-//     console.log("The movie's rating is: " + response.data.imdbRating);
-//   }
-//   .catch(function(error) {
-//     if (error.response) {
-//       console.log(error.response.data.imdbRating);
-//     }
-//   })
-// );
+ 
+// Title of the movie.
+// Year the movie came out.
+// IMDB Rating of the movie.
+// Rotten Tomatoes Rating of the movie.
+// Country where the movie was produced.
+// Language of the movie.
+// Plot of the movie.
+// Actors in the movie.
+// http://www.omdbapi.com/?apikey=[yourkey]&
+function movieThis() {
+    // Basic Node application for requesting data from the OMDB website via axios
+    // Here we incorporate the "axios" npm package
+    var axios = require("axios");
+    
+    // // We then run the request with axios module on a URL with a JSON
+    axios.get("http://www.omdbapi.com/?apikey=2e268f65&t=" + value)
+    .then(
+        function(response) {
+            // Then we print out the imdbRating
+            console.log(response.title);
+            console.log(response.year);
+            console.log(response.ratings.imdbRating);
+            console.log(response.country);
+            console.log(response.language);
+            console.log(response.plot);
+            console.log(response.actors);
+        }
+        .catch(function(error) {
+            if (error.response) {
+                console.log(error.response.title);
+                console.log(error.response.year);
+                console.log(error.response.ratings.imdbRating);
+                console.log(error.response.country);
+                console.log(error.response.language);
+                console.log(error.response.plot);
+                console.log(error.response.actors);
+            }
+        })
+    );
+}
 
 
 // // Includes the FS package for reading and writing packages
-// var fs = require("fs");
+function doWhatItSays() {
+    var fs = require("fs");
 
-// // Running the readFile module that's inside of fs.
-// // Stores the read information into the variable "data"
-// fs.readFile("best_things_ever.txt", text, function(err, data) {
-//   if (err) {
-//     return console.log(err);
-//   }
+    // Running the readFile module that's inside of fs.
+    // Stores the read information into the variable "data"
+    fs.readFile("random.txt", text, function(err, data) {
+        if (err) {
+        return console.log(err);
+    }
+    // Break the string down by comma separation and store the contents into the output array.
+    var output = data.split(",");
+  
+    // Loop Through the newly created output array
+    for (var i = 0; i < output.length; i++) {
+  
+      // Print each element (item) of the array/
+      console.log(output[i]);
+    }
+  });
+}
 
-//   // Break the string down by comma separation and store the contents into the output array.
-//   var output = data.split(",");
 
-//   // Loop Through the newly created output array
-//   for (var i = 0; i < output.length; i++) {
-
-//     // Print each element (item) of the array/
-//     console.log(output[i]);
-//   }
-// });
